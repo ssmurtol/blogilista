3d8edfd36eadd8cc661985a969bf84df27342b5f
ssmurtol <106263318+ssmurtol@users.noreply.github.com>
2022-06-08 11:14:48 +0300

    Initial commit

---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://ssmurtol:${password}@cluster0.xcwb7.mongodb.net/blogs?retryWrites=true`
+
+mongoose.connect(url)
+
+const blogSchema = mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const blog = new Blog({
+    title: "Otsikko",
+    author: "Matti Meikäläinen",
+    url: "http://esimerkki.fi",
+    likes: 100
+})
+
+if ( false ) {
+    blog.save().then(() => {
+    console.log('blog saved!')
+    mongoose.connection.close()
+  })
+}
+
+Blog.find({}).then(result => {
+  result.forEach(blog => {
+    console.log(blog)
+  })
+  mongoose.connection.close()
+})
