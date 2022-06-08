d60c6fa6f35165d94fe2bc24fd313a3acbe48e7b
ssmurtol <106263318+ssmurtol@users.noreply.github.com>
2022-06-08 12:49:30 +0300

    testausta

--- utils/listhelper.js
+++ utils/listhelper.js
@@ -9,7 +9,16 @@ const totalLikes = (blogs) => {
   return sum
 }
 
+const maxLikes = (blogs) => {
+  const likes = blogs.map(blog => blog.likes)
+  const sorted = likes.sort((a, b) => a - b)
+  const maxAmount = sorted[sorted.length - 1]
+  const blogHavingMaxLikes = blogs.find(blog => blog.likes === maxAmount)
+  return blogHavingMaxLikes
+}
+
 module.exports = {
     dummy,
     totalLikes,
+    maxLikes,
 }
